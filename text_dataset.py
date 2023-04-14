9c00a5062087bd1fefcc9cfbd3130d8375aa7865
plawhatre <prashantlawhatre@gmail.com>
2023-04-14 21:50:07 +0530

    added dataset cls for input

---
+++ text_dataset.py
@@ -0,0 +1,12 @@
+from torch.utils.data import Dataset, DataLoader
+
+def TextDataset(Dataset):
+    def __init__(self, src_sent, dst_sent):
+        self.src_sent = src_sent
+        self.dst_sent = dst_sent
+
+    def __len__(self):
+        return len(self.src_sent)
+
+    def __getitem__(self, idx):
+        return self.src_sent[idx], self.dst_sent[idx]
