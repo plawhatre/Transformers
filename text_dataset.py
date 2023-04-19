47d61830ccdafcbd7a28bfaccf9bd98c9b3254d1
plawhatre <prashantlawhatre@gmail.com>
2023-04-19 22:10:26 +0530

    modifed dataset class to get tensor of numbers

--- text_dataset.py
+++ text_dataset.py
@@ -4,9 +4,23 @@ class TextDataset(Dataset):
     def __init__(self, src_sent, dst_sent):
         self.src_sent = src_sent
         self.dst_sent = dst_sent
+        self.src_vocab = self._build_vocab(self.src_sent)
+        self.dst_vocab = self._build_vocab(self.dst_sent)
+
+    def _build_vocab(self, sentences):
+        vocab = {}
+        unique_id = 0
+        for sentence in sentences:
+            for word in sentence.split():
+                if word not in vocab:
+                    vocab[word] = unique_id
+                    unique_id += 1
+        return vocab
 
     def __len__(self):
         return len(self.src_sent)
 
     def __getitem__(self, idx):
-        return self.src_sent[idx], self.dst_sent[idx]
+        src_sent = [self.src_vocab[word] for word in self.src_sent[idx].split()]
+        dst_sent = [self.dst_vocab[word] for word in self.dst_sent[idx].split()]
+        return src_sent, dst_sent
