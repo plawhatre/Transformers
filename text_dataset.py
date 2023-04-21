ecbb5b7f59a3809772cc9b8211f7bf7fead51757
plawhatre <prashantlawhatre@gmail.com>
2023-04-22 00:29:11 +0530

    start and end token

--- text_dataset.py
+++ text_dataset.py
@@ -1,28 +1,39 @@
 from torch.utils.data import Dataset
 
 class TextDataset(Dataset):
-    def __init__(self, src_sent, dst_sent, max_sent_length=50):
+    def __init__(self, src_sent, dst_sent, max_sent_length=50, start_token=True, end_token=True):
         self.src_sent = src_sent
         self.dst_sent = dst_sent
         self.max_sent_length = max_sent_length
+        self.start_token = start_token
+        self.end_token = end_token
 
     @property
     def get_src_vocab(self):
-        return TextDataset._build_vocab(self.src_sent)
+        return TextDataset._build_vocab(self.src_sent, self.start_token)
 
     @property
     def get_dst_vocab(self):
-        return TextDataset._build_vocab(self.dst_sent)
+        return TextDataset._build_vocab(self.dst_sent, self.end_token)
 
     @staticmethod
-    def _build_vocab(sentences):
+    def _build_vocab(sentences, start_token, end_token):
         vocab = {}
-        unique_id = 1
+        if start_token:
+            vocab['START'] = 1
+            unique_id = 2
+        else:
+            unique_id = 1
+
         for sentence in sentences:
             for word in sentence.split():
                 if word not in vocab:
                     vocab[word] = unique_id
                     unique_id += 1
+
+        if end_token:
+            vocab['END'] = unique_id
+            
         return vocab
 
     def __len__(self):
