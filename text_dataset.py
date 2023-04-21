b3d1ada8f0ce4e63470f2d1a520192cf8889e376
plawhatre <prashantlawhatre@gmail.com>
2023-04-22 00:21:42 +0530

    removed tokenization and added properties in text_dataset

--- text_dataset.py
+++ text_dataset.py
@@ -1,14 +1,21 @@
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 
 class TextDataset(Dataset):
     def __init__(self, src_sent, dst_sent, max_sent_length=50):
         self.src_sent = src_sent
         self.dst_sent = dst_sent
-        self.src_vocab = self._build_vocab(self.src_sent)
-        self.dst_vocab = self._build_vocab(self.dst_sent)
         self.max_sent_length = max_sent_length
 
-    def _build_vocab(self, sentences):
+    @property
+    def get_src_vocab(self):
+        return TextDataset._build_vocab(self.src_sent)
+
+    @property
+    def get_dst_vocab(self):
+        return TextDataset._build_vocab(self.dst_sent)
+
+    @staticmethod
+    def _build_vocab(sentences):
         vocab = {}
         unique_id = 1
         for sentence in sentences:
@@ -22,16 +29,4 @@ class TextDataset(Dataset):
         return len(self.src_sent)
 
     def __getitem__(self, idx):
-        src_sent = [self.src_vocab[word] for word in self.src_sent[idx].split()]
-        dst_sent = [self.dst_vocab[word] for word in self.dst_sent[idx].split()]
-        
-        len_src_sent = len(src_sent)
-        len_dst_sent = len(dst_sent)
-
-        if  len_src_sent < self.max_sent_length:
-            src_sent = src_sent + [0]*(self.max_sent_length - len_src_sent)
-
-        if  len_dst_sent < self.max_sent_length:
-            dst_sent = dst_sent + [0]*(self.max_sent_length - len_dst_sent)
-            
-        return src_sent, dst_sent
+        return self.src_sent[idx], self.dst_sent[idx]
