b776472922182d36f96df47e562f7fee461e6b3b
plawhatre <prashantlawhatre@gmail.com>
2023-04-22 20:00:11 +0530

    created rank-4 mask for compatible addition

--- attention.py
+++ attention.py
@@ -15,6 +15,7 @@ class Attention:
     scaled = q@k.transpose(-2,-1) / math.sqrt(d_k)
 
     if mask is not None:
+      mask = mask.unsqueeze(1).repeat(1, scaled.shape[1],1, 1)
       scaled += mask
     attention = F.softmax(scaled, dim=-1)
     values = attention@v
